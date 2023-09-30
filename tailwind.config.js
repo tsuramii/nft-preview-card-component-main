654b57f1b33dbf02d8d7bac1f24ed42001e4f991
Luca Di Marco <lucanahtsu@gmail.com>
2023-09-30 20:36:22 -0300

    Add missing colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,8 @@ module.exports = {
     },
     extend: {
       colors: {
+        'softblue': '#8bacda',
+        'cyan': '#00fff7',
         'mainbg': '#0d192b',
         'cardbg': '#14253d',
         'line': '	#2f415b'
